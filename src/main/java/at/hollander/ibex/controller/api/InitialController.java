917f22e7536e829ed05782714299fe79393612e9
Rene Hollander <mail@renehollander.at>
2018-03-04 15:47:55 +0100

    removed unused import.

--- src/main/java/at/hollander/ibex/controller/api/InitialController.java
+++ src/main/java/at/hollander/ibex/controller/api/InitialController.java
@@ -5,7 +5,6 @@ import at.hollander.ibex.component.UserAccountService;
 import at.hollander.ibex.entity.Account;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
