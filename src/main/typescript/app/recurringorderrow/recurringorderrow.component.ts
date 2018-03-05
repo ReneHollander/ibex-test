3326e47a9bb321209c134afac9c991f9718fa754
Rene Hollander <mail@renehollander.at>
2018-03-05 15:57:44 +0100

    simplified recurring order amount change

--- src/main/typescript/app/recurringorderrow/recurringorderrow.component.ts
+++ src/main/typescript/app/recurringorderrow/recurringorderrow.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, HostListener, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {RecurringOrderItem} from "../shared/models/recurringorder.model";
 
 @Component({
@@ -15,10 +15,6 @@ export class RecurringOrderRowComponent {
     @Input()
     item: RecurringOrderItem;
 
-    edit: boolean = false;
-
-    value: number;
-
     constructor() {
     }
 
@@ -26,34 +22,7 @@ export class RecurringOrderRowComponent {
         this.onDelete.emit(this.item)
     }
 
-    onAmountClick($event: Event) {
-        $event.preventDefault();
-        $event.stopPropagation();
-        if (!this.edit) {
-            this.value = this.item.amount;
-            this.edit = true;
-        }
-    }
-
-    @HostListener('document:click', ['$event']) clickedOutside($event: Event) {
-        this.edit = false;
-    }
-
-    onExitEdit($event: Event) {
-        $event.preventDefault();
-        $event.stopPropagation();
-        this.edit = false;
-    }
-
-    onSaveEdit($event: Event) {
-        $event.preventDefault();
-        $event.stopPropagation();
-        if (this.value == 0) {
-            this.onDelete.emit(this.item);
-        } else {
-            this.item.amount = this.value;
-            this.onUpdate.emit(this.item);
-            this.edit = false;
-        }
+    onAmountChange() {
+        this.onUpdate.emit(this.item);
     }
 }
