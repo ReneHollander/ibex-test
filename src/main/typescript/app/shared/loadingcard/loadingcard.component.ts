391c812ef96d9392c32c4a072f6d39ca44e6da78
Rene Hollander <mail@renehollander.at>
2018-03-10 04:48:06 +0100

    loading card spinner is now centered

--- src/main/typescript/app/shared/loadingcard/loadingcard.component.ts
+++ src/main/typescript/app/shared/loadingcard/loadingcard.component.ts
@@ -2,7 +2,8 @@ import {Component, Input} from '@angular/core';
 
 @Component({
     selector: 'loading-card',
-    templateUrl: './loadingcard.component.html'
+    templateUrl: './loadingcard.component.html',
+    styleUrls: ['./loadingcard.component.css']
 })
 export class LoadingCardComponent {
     @Input() condition: boolean;
