52e20deebd122eaab04920a9a9fb9e4e6681cf7a
Rene Hollander <mail@renehollander.at>
2018-03-11 21:55:55 +0100

    adds validator to check if two form fields are equal

--- src/main/typescript/app/util.ts
+++ src/main/typescript/app/util.ts
@@ -4,6 +4,7 @@ import {Subscriber} from 'rxjs/Subscriber';
 import {OperatorFunction} from 'rxjs/interfaces';
 import {ClassType} from "class-transformer/ClassTransformer";
 import {plainToClass} from "class-transformer";
+import {FormGroup} from "@angular/forms";
 
 export function plainToClassOp<T, R>(cls: ClassType<R>): OperatorFunction<T, R> {
     return function mapOperation(source: Observable<T>): Observable<R> {
@@ -97,5 +98,22 @@ export class CachedValue<T> {
                 resolve(this._value);
             }
         });
+    }
+}
+
+export function validateEqual(key1: string, key2: string) {
+    return (group: FormGroup) => {
+        let control1 = group.controls[key1];
+        let control2 = group.controls[key2];
+        let errors = control2.errors;
+        if (!errors) errors = {};
+        if (control1.value !== control2.value) {
+            errors.equal = false;
+        } else {
+            delete errors.equal;
+            if (Object.keys(errors).length == 0) errors = null;
+        }
+        if (control2.pristine && !control1.pristine) control2.markAsDirty();
+        control2.setErrors(errors);
     }
 }
