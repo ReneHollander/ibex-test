bb8387f1f1c25ac36d50fb5b13e024f1c5b50c9c
Rene Hollander <mail@renehollander.at>
2018-04-03 02:17:40 +0200

    fixed build information not getting read from manifest

--- src/main/java/at/hollander/ibex/configuration/BuildInformationConfiguration.java
+++ src/main/java/at/hollander/ibex/configuration/BuildInformationConfiguration.java
@@ -1,31 +1,29 @@
 package at.hollander.ibex.configuration;
 
 import at.hollander.ibex.pojo.BuildInformation;
+import com.jcabi.manifests.Manifests;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
 
 @Configuration
 public class BuildInformationConfiguration {
 
+    private static String get(String key, String defaultValue) {
+        return Manifests.exists(key) ? Manifests.read(key) : defaultValue;
+    }
+
     @Bean
     public BuildInformation buildInformation() throws IOException {
-        InputStream is = this.getClass().getClassLoader().getResourceAsStream("META-INF/MANIFEST.MF");
-
-        Properties prop = new Properties();
-        prop.load(is);
-
         return BuildInformation.builder()
-                .longHash(prop.getProperty("Ibex-Build-Long-Hash", "0000000000000000000000000000000000000000"))
-                .shortHash(prop.getProperty("Ibex-Build-Short-Hash", "00000000"))
-                .branch(prop.getProperty("Ibex-Build-Branch", "no_branch_supplied"))
-                .jobId(Integer.parseInt(prop.getProperty("Ibex-Build-Job-Id", "0")))
-                .versionTag(prop.getProperty("Ibex-Build-Version-Tag", "00000000-0"))
-                .time(prop.getProperty("Ibex-Build-Time", "1970-01-01T00:00:00Z"))
-                .timeMillis(Long.parseLong(prop.getProperty("Ibex-Build-Time-Millis", "0")))
+                .longHash(get("Ibex-Build-Long-Hash", "0000000000000000000000000000000000000000"))
+                .shortHash(get("Ibex-Build-Short-Hash", "00000000"))
+                .branch(get("Ibex-Build-Branch", "no_branch_supplied"))
+                .jobId(Integer.parseInt(get("Ibex-Build-Job-Id", "0")))
+                .versionTag(get("Ibex-Build-Version-Tag", "00000000-0"))
+                .time(get("Ibex-Build-Time", "1970-01-01T00:00:00Z"))
+                .timeMillis(Long.parseLong(get("Ibex-Build-Time-Millis", "0")))
                 .build();
     }
 
